d2d22c7de268a9f44d453a5c999d1dbcd827cc0a
David Jones <djones6@uk.ibm.com>
2020-06-08 15:14:05 +0100

    Merge Appsody project with existing project

--- voyages-ms/server/server.js
+++ voyages-ms/server/server.js
@@ -16,30 +16,34 @@ const path = require('path');
 
 const logger = log4js.getLogger(appName);
 logger.level = process.env.LOG_LEVEL || 'info'
-const app = express();
-const server = http.createServer(app);
 
-app.use(express.json());
-app.use(log4js.connectLogger(logger, { level: logger.level }));
-
-
-require('./services/index')(app);
-require('./routers/index')(app, server);
-
-const port = localConfig.getPort();
-const kbroker = localConfig.getKafkaBrokers();
-server.listen(port, function(){
-  logger.info(`voyagesms listening on http://localhost:${port}/appmetrics-dash`);
-  logger.info(`voyagesms listening on http://localhost:${port}`);
-  logger.info(`voyagesms should be connected to kafka at: ${kbroker}`  );
-});
-
-app.use(function (req, res, next) {
-  res.sendFile(path.join(__dirname, '../public', '404.html'));
-});
-
-app.use(function (err, req, res, next) {
-	res.sendFile(path.join(__dirname, '../public', '500.html'));
-});
-
-module.exports = server;
+module.exports = (options) => {
+  const app = express();
+//  const server = http.createServer(app);
+    
+  app.use(express.json());
+  app.use(log4js.connectLogger(logger, { level: logger.level }));
+  
+  require('./services/index')(app);
+  require('./routers/index')(app);
+  
+  const port = localConfig.getPort();
+  const kbroker = localConfig.getKafkaBrokers();
+  // server.listen(port, function(){
+  //   logger.info(`voyagesms listening on http://localhost:${port}/appmetrics-dash`);
+  //   logger.info(`voyagesms listening on http://localhost:${port}`);
+  //   logger.info(`voyagesms should be connected to kafka at: ${kbroker}`  );
+  // });
+  
+  app.use(function (req, res, next) {
+    res.status(404).sendFile(path.join(__dirname, '../public', '404.html'));
+  });
+  
+  app.use(function (err, req, res, next) {
+    res.status(500).sendFile(path.join(__dirname, '../public', '500.html'));
+  });
+  
+  // module.exports = server;
+  
+  return app;
+};
