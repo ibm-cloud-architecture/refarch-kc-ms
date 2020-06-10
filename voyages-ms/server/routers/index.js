5261b6eeda7d186e0192931ec8bb14119098f601
David Jones <djones6@uk.ibm.com>
2020-06-10 10:55:03 +0100

    Remove hack, hook into http server close event

--- voyages-ms/server/routers/index.js
+++ voyages-ms/server/routers/index.js
@@ -2,5 +2,4 @@ module.exports = function(app){
     require('./public')(app);
     require('./health')(app);
     require('./voyage')(app);
-    require('./shutdown')(app);
 };
--- voyages-ms/server/server.js
+++ voyages-ms/server/server.js
@@ -16,9 +16,10 @@ const path = require('path');
 const logger = log4js.getLogger(appName);
 logger.level = process.env.LOG_LEVEL || 'info'
 
+const kafka = require('./utils/kafka.js');
+
 module.exports = (options) => {
   const app = express();
-//  const server = http.createServer(app);
     
   app.use(express.json());
   app.use(log4js.connectLogger(logger, { level: logger.level }));
@@ -26,14 +27,14 @@ module.exports = (options) => {
   require('./services/index')(app);
   require('./routers/index')(app);
   
-  const port = localConfig.getPort();
   const kbroker = localConfig.getKafkaBrokers();
-  // server.listen(port, function(){
-  //   logger.info(`voyagesms listening on http://localhost:${port}/appmetrics-dash`);
-  //   logger.info(`voyagesms listening on http://localhost:${port}`);
-  //   logger.info(`voyagesms should be connected to kafka at: ${kbroker}`  );
-  // });
+  logger.info(`voyagesms should be connected to kafka at: ${kbroker}`);
   
+  // Disconnect Kafka clients when the HTTP server is closed.
+  options.server.on('close', function () {
+    kafka.disconnect();
+  });
+
   app.use(function (req, res, next) {
     res.status(404).sendFile(path.join(__dirname, '../public', '404.html'));
   });
@@ -41,8 +42,6 @@ module.exports = (options) => {
   app.use(function (err, req, res, next) {
     res.status(500).sendFile(path.join(__dirname, '../public', '500.html'));
   });
-  
-  // module.exports = server;
   
   return app;
 };
